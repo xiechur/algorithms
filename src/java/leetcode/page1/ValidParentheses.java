a99610dfa88b2fca1ecb8f640bb8784a38133a83
xiechur <xiechur@gmail.com>
2019-05-20 23:47:27 +0800

    ValidParentheses

---
+++ src/java/leetcode/page1/ValidParentheses.java
@@ -0,0 +1,25 @@
+package java.leetcode.page1;
+
+public class ValidParentheses {
+
+    public boolean isValid(String s) {
+        if(s == null || s.length() % 2!=0) {
+            return false;
+        }
+        Stack<Character> stack = new Stack<Character>();
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                stack.push(')');
+            }
+            else if(s.charAt(i) == '{'){
+                stack.push('}');
+            }
+            else if(s.charAt(i) == '['){
+                stack.push(']');
+            }
+            else if(stack.isEmpty() || stack.pop() != s.charAt(i))
+                return false;
+        }
+        return stack.empty();
+    }
+}

b2705065459238f2db4e971a08fa9865fff7b7d3
xiechur <xiechur@gmail.com>
2019-05-23 23:33:34 +0800

    RomanToInt

--- src/java/leetcode/RomanToInt.java
+++ src/java/leetcode/RomanToInt.java
@@ -1,27 +1,22 @@
 package java.leetcode;
 
 public class RomanToInt {
-
-    public int romanToInt(String s) {
-        class Solution {
-            public boolean isPalindrome(int x) {
-                int num = 0;
-                int temp = x;
-                if(x== 0){
-                    return true;
-                }else if(x < 0){
-                    return false;
-                }else if (x < 10){
-                    return true;
-                }else{
-                    while(x > 0) {
-                        num = num * 10 + x%10;
-                        x /= 10;
-                    }
-                }
-                return temp == num;
+    public boolean isPalindrome(int x) {
+        int num = 0;
+        int temp = x;
+        if (x == 0) {
+            return true;
+        } else if (x < 0) {
+            return false;
+        } else if (x < 10) {
+            return true;
+        } else {
+            while (x > 0) {
+                num = num * 10 + x % 10;
+                x /= 10;
             }
         }
+        return temp == num;
     }
 
 }

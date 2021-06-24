f3768edf7ee53e8ac484ea10553465d5cde80953
xiechurong <xiechurong@huya.com>
2021-06-24 17:46:57 +0800

    adapter

---
+++ src/java/designPattern/adapter/Client.java
@@ -0,0 +1,30 @@
+package designPattern.adapter;
+
+/**
+ * 适配器模式
+ * 将两个不兼容的接口之间适配
+ *
+ * spring AOP中的适配器模式
+ * 我们知道 Spring AOP 的实现是基于代理模式，但是 Spring AOP 的增强或通知(Advice)使用到了适配器模式，与之相关的接口是AdvisorAdapter 。Advice 常用的类型有：BeforeAdvice（目标方法调用前,前置通知）、AfterAdvice（目标方法调用后,后置通知）、AfterReturningAdvice(目标方法执行结束后，return之前)等等。每个类型Advice（通知）都有对应的拦截器:MethodBeforeAdviceInterceptor、AfterReturningAdviceAdapter、AfterReturningAdviceInterceptor。Spring预定义的通知要通过对应的适配器，适配成 MethodInterceptor接口(方法拦截器)类型的对象（如：MethodBeforeAdviceInterceptor 负责适配 MethodBeforeAdvice）。
+ *
+ * spring MVC中的适配器模式
+ * 在Spring MVC中，DispatcherServlet 根据请求信息调用 HandlerMapping，解析请求对应的 Handler。解析到对应的 Handler（也就是我们平常说的 Controller 控制器）后，开始由HandlerAdapter 适配器处理。HandlerAdapter 作为期望接口，具体的适配器实现类用于对目标类进行适配，Controller 作为需要适配的类。
+ *
+ * 为什么要在 Spring MVC 中使用适配器模式？ Spring MVC 中的 Controller 种类众多，不同类型的 Controller 通过不同的方法来对请求进行处理。如果不利用适配器模式的话，DispatcherServlet 直接获取对应类型的 Controller，需要的自行来判断，像下面这段代码一样：
+ *
+ * 场景：
+ *
+ * 实现：
+ * 参考博客：https://www.cnblogs.com/kyoner/p/10949246.html
+ *
+ *
+ * @author xiechurong
+ * @Date 2021/6/22
+ */
+public class Client {
+
+
+
+
+
+}
